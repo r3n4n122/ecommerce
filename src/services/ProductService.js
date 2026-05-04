58f2f367385b9951fde314fb84dfa1b376c2b2e4
r3n4n122 <renan.goncalves@3clicksrh.com.br>
2026-05-04 19:54:37 -0300

    feat add category and search

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -3,18 +3,30 @@ import { getInstance } from "./fetchApiService.js";
 export const listProducts = async ({limit, page, search, category}) => {
   const skip = (page - 1) * limit;
 
+  let path = ''
+
+  if(search){
+    path = `/products/search?q=${search}&limit=${limit || 10}&skip=${skip || 0}`
+  }else if(category){
+    path = `/products/category/${category}?limit=${limit || 10}&skip=${skip || 0}`
+    console.log(path)
+  }else{
+    path = `/products?limit=${limit || 10}&skip=${skip || 0}`
+  }
+
   const results = await getInstance({
-    path: `/products?limit=${limit || 10}&skip=${skip || 0}`,
+    path: path,
   })
 
   const products = {
     page: page,
     limit: results.data.limit,
     total: results.data.total,
-    data: results.data.products.map((item => (
+    data: (results.data.products || []).map((item => (
       {
         id: item.id,
         name: item.title,
+        category: item.category,
         price: item.price,
         stock: item.stock,
         rating: item.rating,
