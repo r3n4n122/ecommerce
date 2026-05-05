e28e1629ed0e0ac9daf314a39ed937f3124791a4
r3n4n122 <renan.goncalves@3clicksrh.com.br>
2026-05-05 02:40:08 -0300

    removed stack

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,33 +1,28 @@
 export const errorMiddleware = (err, req, res, next) => {
   const status = err.status || 500
   const isProduction = process.env.NODE_ENV === 'production';
-
+  
   switch(status){
     case 400:
       return res.status(status).json({
-        message: err.message || "Parâmetro invalido",
-        stack: isProduction ? undefined : err.stack,
+        message: err.message || "Parâmetro invalido"
       })
     case 401:
       return res.status(status).json({
-        message: err.message || "usuário ou senha inválido",
-        stack: isProduction ? undefined : err.stack,
+        message: err.message || "usuário ou senha inválido"
       })
     case 404:
       return res.status(status).json({
-        message: err.message || "Página não encontrada.",
-        stack: isProduction ? undefined : err.stack,
+        message: err.message || "Página não encontrada."
       })
     case 500:
       console.log(err)
       return res.status(status).json({
-        message: "Erro interno no servidor",
-        stack: isProduction ? undefined : err.stack,
+        message: "Erro interno no servidor"
       })
     default: 
       return res.status(status).json({
-        message: "Ocorreu um erro inesperado. Tente novamente mais tarde.",
-        stack: isProduction ? undefined : err.stack,
+        message: "Ocorreu um erro inesperado. Tente novamente mais tarde."
       })
   }
 };
